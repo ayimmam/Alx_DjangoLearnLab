4392f6fc682078a28012a4f96482dbe19457e6f6
ayimmam <anteneh.yimmam@gmail.com>
2025-09-08 19:44:44 +0300

    feat: implement admin functionality

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    
+    list_display = ('title', 'author', 'publication_year')
+    
+    # Add a list filter sidebar to easily filter books by publication year.
+    list_filter = ('publication_year',)
+    
+    # Enable a search bar to search for books by their title or author.
+    search_fields = ('title', 'author')
+
+# Register the Book model with the custom admin class and use this configuration
+admin.site.register(Book, BookAdmin)
