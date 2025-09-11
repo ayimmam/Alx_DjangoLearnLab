688d2f6d661905311eaa68a2827f7e7ad33465fc
ayimmam <anteneh.yimmam@gmail.com>
2025-09-11 20:17:49 +0300

    Implementing Advanced Model Relationships

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,5 +1,7 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
+Library_name = 'Central Library'
+
 # Sample Data 
 author1, created = Author.objects.get_or_create(name="J.K. Rowling")
 book1, created = Book.objects.get_or_create(title="Harry Potter and the Sorcerer's Stone", author=author1)
@@ -8,11 +10,10 @@ book2, created = Book.objects.get_or_create(title="Harry Potter and the Chamber 
 author2, created = Author.objects.get_or_create(name="George Orwell")
 book3, created = Book.objects.get_or_create(title="1984", author=author2)
 
-library1, created = Library.objects.get_or_create(name="Library_name")
+library1, created = Library.objects.get_or_create(name=Library_name)
 library1.books.add(book1, book3)
 
 librarian1, created = Librarian.objects.get_or_create(name="Jane Doe", library=library1)
-
 print("--- Query 1: All books by a specific author ---")
 # Use the reverse relationship from Author to Book
 for book in Author.objects.get(name="J.K. Rowling").books.all():
@@ -21,12 +22,12 @@ print("-" * 20)
 
 print("--- Query 2: All books in a library ---")
 # Use the Many-to-Many relationship
-for book in Library.objects.get(name="Library_name").books.all():
+for book in Library.objects.get(name=Library_name).books.all():
     print(book.title)
 print("-" * 20)
 
 print("--- Query 3: The librarian for a specific library ---")
 # Use the reverse One-to-One relationship
-librarian_name = Library.objects.get(name="Library_name")
+librarian_name = Library.objects.get(name=Library_name).books.all()
 print(f"The librarian is {librarian_name}")
 print("-" * 20)
