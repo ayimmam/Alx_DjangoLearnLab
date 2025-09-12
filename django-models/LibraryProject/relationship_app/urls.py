fa4c64d7028aff8e6faf692cba7ef0ad84058aa4
ayimmam <anteneh.yimmam@gmail.com>
2025-09-12 20:41:43 +0300

    fix: urls formating

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,12 @@
-from .views import list_books, LibraryDetailView, register, UserLoginView, UserLogoutView
+from . import views
 from django.urls import path
 
 
 urlpatterns = [
-   path('books/', list_books, name='list_books'),
-   path('library/', LibraryDetailView.as_view(), name='library_detail'),
-   path('register/', register, name='register'),
-   path('login/', UserLoginView.as_view(), name='login'),
-   path('logout/', UserLogoutView.as_view(), name='logout'),
+   path('books/', views.list_books, name='list_books'),
+   path('library/', views.LibraryDetailView.as_view(), name='library_detail'),
+   path('register/', views.register, name='register'),
+   path('login/', views.UserLoginView.as_view(), name='login'),
+   path('logout/', views.UserLogoutView.as_view(), name='logout'),
 
 ]
