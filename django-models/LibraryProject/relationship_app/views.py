88efa40bfca34cf40fdcfb8c1e12672749c204c2
ayimmam <anteneh.yimmam@gmail.com>
2025-09-12 19:49:49 +0300

    Implement function based view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Book, Author, Library, Librarian
 
-# Create your views here.
+
+def library_view(request):
+   
+    books = Book.objects.all()  # Fetch all book instances from the database
+    authors = Author.objects.all()  # Fetch all author instances from the database
+    context = {'book_list': books, 'author_list': authors}  # Create a context dictionary with book and author lists
+    return render(request, 'books/book_list.html', context)
