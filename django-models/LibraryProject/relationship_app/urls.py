c4c29d7b764f0f09c715f30715abe506f7d28777
ayimmam <anteneh.yimmam@gmail.com>
2025-09-16 18:14:30 +0300

    fix: implementing the provided auth templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,9 +5,9 @@ from . import views
 urlpatterns = [
    path('books/', book_list, name='list_books'),
    path('library/', LibraryDetailView.as_view(), name='library_detail'),
-     path('register/', views.register, name='register'),
-    path('login/', UserLoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', UserLogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
     # Role-based views
     path('admin_dashboard/', views.admin_view, name='admin_view'),
     path('librarian_dashboard/', views.librarian_view, name='librarian_view'),
